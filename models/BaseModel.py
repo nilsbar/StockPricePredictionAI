956a72ab5cd7ea0f514c333d78c4a42e7a8fc90d
Nils <63423070+nilsbar@users.noreply.github.com>
2023-12-25 19:41:13 +0100

    BaseModel scheme designed

---
+++ models/BaseModel.py
@@ -0,0 +1,13 @@
+
+class BaseModel:
+    """
+        This is the parent class of all models.
+    """
+    def __init__(self) -> None:
+        pass
+    
+    def predict():
+        pass
+
+    def train():
+        pass
