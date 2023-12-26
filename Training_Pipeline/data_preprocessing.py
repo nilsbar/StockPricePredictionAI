868e8f0e77847ff03d5214b7a1ac0a4da0f16c8a
Nils <63423070+nilsbar@users.noreply.github.com>
2023-12-26 16:38:18 +0100

    basic scheme

---
+++ Training_Pipeline/data_preprocessing.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+
+class DataPreprocessing():
+    """
+        A class for preprocessing and gathering the data for model training. 
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def _datagathering(self):
+        """
+        Gather the raw data through downloading with the yfinance package.
+        """
+        pass
+
+    def _preprocessing(self):
+        """
+        Preprocess the data for models.
+        """
+        pass
